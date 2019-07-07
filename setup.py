d3b270e2c2689869c054ff3e3598de4c376377bd
Laura McLoughlin <lauramccomish@hotmail.com>
2019-07-07 18:23:59 +0100

    update to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
     name="git_test_pkg_lauramcloughlin",
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Laura McLoughlin",
     author_email="b00092153@student.itb.ie",
     description="A text misclassification analysis package",
-    long_description=long_description,
+    long_description="long_description",
     long_description_content_type="text/markdown",
     url="https://github.com/lauramcloughlin/git_test_pkg",
     packages=setuptools.find_packages(),
