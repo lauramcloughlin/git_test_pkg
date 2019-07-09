6f9cabf056bfd6ee95dda8d0a6e4cb1cc7f99e74
Laura McLoughlin <lauramccomish@hotmail.com>
2019-07-09 19:59:55 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="git_test_pkg_lauramcloughlin",
-    version="0.0.1",
-    author="Laura McLoughlin",
-    author_email="b00092153@student.itb.ie",
-    description="A text misclassification analysis package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/lauramcloughlin/git_test_pkg",
-    packages=setuptools.find_packages(),
-    classifiers=[
+setup(
+  name = 'git_test_pkg',
+  version = '0.0.1',
+  description = 'A text misclassification analysis package.',
+  author = 'lauramcloughlin',
+  author_email = 'b00092153@student.itb.ie',
+  url = 'https://github.com/lauramcloughlin/py_git_test',
+  download_url = 'https://github.com/lauramcloughlin/py_git_test/archive/1.4.12.tar.gz',
+  keywords = ['py_git_test', 'misclassification', 'classification', 'text'],
+  packages = ['py_git_test'],
+  classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+  install_requires=[]
 )
